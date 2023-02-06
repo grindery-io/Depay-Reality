560b6eb4c15f72564c9354df89a211900ed09a31
sam <ayorindesam2@gmail.com>
2023-02-06 12:12:44 +0100

    test script

--- test/GRT-pool.ts
+++ test/GRT-pool.ts
@@ -13,7 +13,7 @@ describe("GRTPOOL", function () {
   let userC: any;
   let userD: any;
 
-  before(async function () {
+  this.before(async function () {
     [userA, userB, userC, userD] = await ethers.getSigners();
     const _realityEth = await ethers.getContractFactory("RealityETH_v3_0");
     const REALITYETH = await _realityEth.deploy();
@@ -40,20 +40,23 @@ describe("GRTPOOL", function () {
 
     // initialize contract
     await GRTPOOL.initialize()
+
     // set exchange rate with withdrawal token
     await GRTPOOL.setExchangeRate(TokenToWithdraw.address, 2)
+
     // Mint withdrawal tokens for GRTPOOL contract
     await TokenToWithdraw.mint(GRTPOOL.address, '1000000000000000000000')
+
     expect(await TokenToWithdraw.balanceOf(GRTPOOL.address)).to.equal(
       '1000000000000000000000'
     );
-
     await GRTToken.mint(
       userA.address, '1000000000000000000000');
 
     expect(await GRTToken.balanceOf(userA.address)).to.equal(
       '1000000000000000000000'
     );
+
     await GRTToken.approve(GRTPOOL.address, '1000000000000000000000')
 
     // transfer ERC20 to pool by UserA
@@ -64,23 +67,30 @@ describe("GRTPOOL", function () {
     const stringTemplate = '{"title": "%s", "type": "bool", "category": "%s", "lang": "%s"}'
     const id = await GRTPOOL.createRealityERC20Template(stringTemplate)
     const _id = await id.wait()
-    const templateId = _id?.events[1]?.args[0].toString()
+    const templateId = _id.events[1].args[0].toString()
     console.log(templateId.toString())
     const questionStr = 'Did I make a transaction deposit of 1000GRT?␟␟en'
     const __questionId = await GRTPOOL.createQuestionERC20(questionStr, txhash, 2, 1000, TokenToWithdraw.address, userB.address, templateId, { value: ethers.utils.parseEther("0.1") });
     const _questionId = await __questionId.wait()
-    const questionId = _questionId?.events[2]?.args[0]
+    const questionId = _questionId.events[2].args[0]
+    console.log(questionId)
+
+    console.log('before b')
+
+    const getHistoryHash = await GRTPOOL.getHistoryHash(questionId)
+    console.log('firstHistory', getHistoryHash)
 
 
     // answering a question
-    await GRTPOOL.connect(userC).answerQuestion('true', questionId, 1, { value: ethers.utils.parseEther("1") });
-    await GRTPOOL.connect(userD).answerQuestion('false', questionId, 1, { value: ethers.utils.parseEther("2") });
+    await GRTPOOL.connect(userC).answerQuestion('true', questionId, 0, { value: ethers.utils.parseEther("1") });
+    const getHistoryHash1 = await GRTPOOL.getHistoryHash(questionId)
+    console.log('getHistoryHash1', getHistoryHash1)
+
+    await GRTPOOL.connect(userD).answerQuestion('false', questionId, 0, { value: ethers.utils.parseEther("2") });
 
     // const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
     // const network = { provider: provider };
 
-    // console.log('provider', network)
-    // //  const signer = await hardhatEthers.getSigner();
 
     await network.provider.send("evm_increaseTime", [97200])
     await network.provider.send("evm_mine")
@@ -96,16 +106,27 @@ describe("GRTPOOL", function () {
     const getBounty = await GRTPOOL.getBounty(questionId)
     console.log(getBounty.toString())
 
-    const getHistoryHash = await GRTPOOL.getHistoryHash(questionId)
-    console.log(getHistoryHash)
+    const getHistoryHash2 = await GRTPOOL.getHistoryHash(questionId)
+    console.log('getHistoryHash2', getHistoryHash2)
 
-    const answer = await GRTPOOL.getBytes(questionStr);
+    const answer1 = await GRTPOOL.getBytes('true');
+    console.log(answer1);
+
+    const answer2 = await GRTPOOL.getBytes('false');
+    console.log(answer2);
+
+
+    // const verifyHistoryInputOrRevert = await GRTPOOL.verifyHistoryInputOrRevert(getHistoryHash1, getHistoryHash, answer1, "1000000000000000000", userC.address)
+    // console.log('verifyHistoryInputOrRevert', verifyHistoryInputOrRevert)
+
     const canWithdrawA = await GRTPOOL.canUserWithdraw(userB.address, questionId)
     console.log('a', canWithdrawA)
-    await GRTPOOL.claimWinnings(questionId, [getHistoryHash], [userC.address], ['100000000000000000'], [answer])
+    await GRTPOOL.claimWinnings(questionId, [getHistoryHash1, getHistoryHash], [userD.address, userC.address], [ethers.utils.parseEther("2"), ethers.utils.parseEther("1")], [answer2, answer1])
 
     const canWithdrawB = await GRTPOOL.canUserWithdraw(userB.address, questionId)
     console.log(canWithdrawB)
   });
+
+  
 });
 
