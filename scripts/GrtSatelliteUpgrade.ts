b98fe8cb21e28e703d80a41c5d88f68260c3e90f
Thomas Coratger <thomas.coratger@gmail.com>
2023-02-22 00:24:37 +0100

    test and upgrade satellite

--- scripts/GrtSatelliteUpgrade.ts
+++ scripts/GrtSatelliteUpgrade.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from "hardhat";
 
-const GRT_SATELLITE_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const GRT_SATELLITE_ADDRESS = "0xef468d8bde6cab3e678587415226a3d4baac5f01";
 
 async function main() {
 
