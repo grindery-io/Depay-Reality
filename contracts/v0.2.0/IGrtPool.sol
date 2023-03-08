910f970482ad01aa53c1c6479128eaf86d77e30b
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-03-08 11:48:06 -0300

    remove unused interfaces

--- contracts/v0.2.0/IGrtPool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.17;
-
-interface IGrtPool {
-    function getStatusOffer(bytes32 idOffer) external view returns (bool);
-    function getOfferer(bytes32 idOffer) external view returns (address);
-    function deposit(uint256 amount) external;
-}
--- contracts/v0.2.0/IGrtSatellite.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.17;
-
-interface IGrtSatellite {
-    function rewardOffer(bytes32 idOffer, uint256 amount) external returns (bool); 
-}
