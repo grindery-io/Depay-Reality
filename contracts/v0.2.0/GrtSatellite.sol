f25ed35280646dc5d7a6928c32bdd08123792a0a
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-03-09 15:27:28 -0300

    remove line

--- contracts/v0.2.0/GrtSatellite.sol
+++ contracts/v0.2.0/GrtSatellite.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "./GrtLiquidityWallet.sol";
-
 import "hardhat/console.sol";
 
 contract GrtSatellite is OwnableUpgradeable {
